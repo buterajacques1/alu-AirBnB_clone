f516e7339cd2c9ac142555d8f026e9da422560c0
buterajacques1 <jbutera702@gmail.com>
2023-02-10 19:20:27 +0200

    updated class model

--- models/base_model.py
+++ models/base_model.py
@@ -14,14 +14,12 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
         return '[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
-        models.storage.save()
 
     def to_dict(self):
         result = self.__dict__.copy()
