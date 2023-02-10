2151b8206cf9bafe628f7fe4b50051bd8d376302
buterajacques1 <jbutera702@gmail.com>
2023-02-10 19:14:13 +0200

    updated class model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,36 +1,37 @@
 import unittest
-from models.base_model import BaseModel
 from datetime import datetime
-
+import models
+import uuid
 
 class TestBaseModel(unittest.TestCase):
     def setUp(self):
-        self.base_model = BaseModel()
+        self.model = models.BaseModel()
 
     def test_init(self):
-        self.assertIsInstance(self.base_model, BaseModel)
-        self.assertIsInstance(self.base_model.id, str)
-        self.assertIsInstance(self.base_model.created_at, datetime)
-        self.assertIsInstance(self.base_model.updated_at, datetime)
+        self.assertIsInstance(self.model, models.BaseModel)
+        self.assertIsInstance(self.model.id, str)
+        self.assertIsInstance(self.model.created_at, datetime)
+        self.assertIsInstance(self.model.updated_at, datetime)
 
     def test_str(self):
-        model = BaseModel()
-        expected_output = '[BaseModel] ({}) {{}}'.format(model.id)
-        self.assertEqual(str(model)[:len(expected_output)], expected_output)
+        expected_str = '[BaseModel] ({}) {{'format(self.model.id)
+        expected_str += "'id': '{}', ".format(self.model.id)
+        expected_str += "'created_at': {}, ".format(repr(self.model.created_at))
+        expected_str += "'updated_at': {}}}".format(repr(self.model.updated_at))
+        self.assertEqual(str(self.model), expected_str)
 
     def test_save(self):
-        previous_update_time = self.base_model.updated_at
-        self.base_model.save()
-        self.assertNotEqual(previous_update_time, self.base_model.updated_at)
+        updated_at = self.model.updated_at
+        self.model.save()
+        self.assertNotEqual(updated_at, self.model.updated_at)
 
     def test_to_dict(self):
-        dictionary = self.base_model.to_dict()
-        self.assertIsInstance(dictionary, dict)
-        self.assertEqual(dictionary["__class__"], "BaseModel")
-        self.assertIsInstance(dictionary["created_at"], str)
-        self.assertIsInstance(dictionary["updated_at"], str)
+        model_dict = self.model.to_dict()
+        self.assertIsInstance(model_dict, dict)
+        self.assertEqual(model_dict['__class__'], 'BaseModel')
+        self.assertEqual(model_dict['id'], self.model.id)
+        self.assertEqual(model_dict['created_at'], self.model.created_at.isoformat())
+        self.assertEqual(model_dict['updated_at'], self.model.updated_at.isoformat())
 
-
-if __name__ == "__main__":
-
+if __name__ == '__main__':
     unittest.main()
